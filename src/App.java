c89be0125b0f2b3fa78e12d08e1ab81d4c0e144b
JUANPABLOQUIPAZOCA <1torettos@gmail.com>
2021-07-07 16:51:30 -0500

    second class, java coding

--- src/App.java
+++ src/App.java
@@ -1,5 +1,48 @@
+import org.jcp.xml.dsig.internal.SignerOutputStream;
+
 public class App {
+
+    private String nombre;
+    private int edad;
+    private char sexo;
+    private double estatura;
+    private float peso;
+    private boolean estadoCivil;
+    
+
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        //tipo de datos y operadores
+        String res = "hola";
+        int resultado= 15 + 32;
+        int resultado1 = 125 - 58;
+        double resultado2 = 568 / 6;
+        float resultado3 = 58 * 21;
+        int resultado4 = 21 % 9 ;
+
+        //operadores unarios
+        resultado++; // resultado = resultado + 1
+        resultado--; // resultado = resultado - 1
+        resultado= -resultado; //valor negativo
+        resultado= +resultado; // valor absoluto
+
+        //operadores logicos
+        boolean respuesta = true;
+        respuesta = 5 < 8; // operador menor que
+        respuesta = 5 > 8; // operador mayor que
+        respuesta = 5 >= 8; // operador mayoro igual que
+        respuesta = 5 <= 8; // operador menor o igual que
+        respuesta = 5 != 8; // operador diferente o negar que 
+        respuesta = 5 == 8; // operador si es igual que
+        respuesta = 12 < 5 && 4 > 1; // operador unir o y en la condicion
+        respuesta = 14 < 25 || 1 > 5; // operador or 
+
+        // operadores bit a bit
+        
+
+        sout
+
     }
 }
